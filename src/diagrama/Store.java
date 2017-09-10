fa62836fcfb613495c09711dc4a109a880a5e78c
mandradep <30605024+mandradep@users.noreply.github.com>
2017-09-09 20:32:25 -0500

    Update Store.java

--- src/diagrama/Store.java
+++ src/diagrama/Store.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package diagrama;
 
-/**
- *
- * @author Giova
- */
+
 public class Store {
    private Address addreess;
    private Car  carros[]=new Car[20];
